a95819ffd36581b000ffee0cace8f179897c0ce6
Pierre Rochard <pierre@rochard.org>
2017-11-24 22:26:25 -0500

    initial commit

---
+++ readingbitcoin/articles.py
@@ -0,0 +1,10 @@
+articles_data = [
+    {
+        'id': 0,
+        'file_name': 'why-run-a-full-node',
+        'title': 'Why Run a Full Node',
+        'author': 'Pieter Wuille',
+        'source': 'https://www.reddit.com/r/BitcoinBeginners/comments/3eq3y7/full_node_question/ctk4lnd/',
+        'preview': "One of Bitcoin's strengths - the most important in my opinion even - is the low degree of trust you need in others. If you use a full node for your incoming transactions, you know that there was no cheating anytime in the history of your coins..."
+    }
+]
---
+++ readingbitcoin/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+from readingbitcoin.articles import articles_data
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/fonts/<font_name>')
+def fonts(font_name):
+    return ''
+
+
+@app.route('/articles/')
+def articles():
+    return render_template('articles.html', articles_data=articles_data)
+
+
+@app.route('/articles/<file_name>/')
+def article(file_name):
+    article_data = [a for a in articles_data if a['file_name'] == file_name][0]
+    return render_template(
+        'articles/{file_name}.html'.format(file_name=file_name),
+        article_data=article_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7809)
