431e2d6bf412e483525c0964fbaf69a4adcab3a3
Pierre Rochard <pierre@rochard.org>
2017-12-03 17:22:04 -0500

    adding segwit2x bugs explained

--- readingbitcoin/articles.py
+++ readingbitcoin/articles.py
@@ -9,5 +9,16 @@ articles_data = [
         'author': 'Pieter Wuille',
         'source': 'https://www.reddit.com/r/BitcoinBeginners/comments/3eq3y7/full_node_question/ctk4lnd/',
         'preview': "One of Bitcoin's strengths - the most important in my opinion even - is the low degree of trust you need in others. If you use a full node for your incoming transactions, you know that there was no cheating anytime in the history of your coins..."
+    },
+    {
+        'id': 1,
+        'month': 12,
+        'year': 2017,
+        'image_name': 'jimmysong.jpeg',
+        'file_name': 'segwit2x-bugs-explained',
+        'title': 'Segwit2x Bugs Explained',
+        'author': 'Jimmy Song',
+        'source': 'https://bitcointechtalk.com/segwit2x-bugs-explained-8e0c286124bc',
+        'preview': "There was a bug in the Segwit2x software which caused the client to stop at block 494782. Examine the details of what caused the software to stop..."
     }
 ]
