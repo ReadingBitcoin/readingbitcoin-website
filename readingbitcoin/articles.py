1e985725b9801ec05f9df7f758cb256234492fc4
Pierre Rochard <pierre@rochard.org>
2018-03-23 02:12:38 -0400

    adding article dates

--- readingbitcoin/articles.py
+++ readingbitcoin/articles.py
@@ -1,35 +1,41 @@
 articles_data = [
     {
-        'id': 0,
+        'id': 2,
+        'day': 20,
         'month': 11,
-        'year': 2017,
-        'image_name': 'pwuille.png',
-        'file_name': 'why-run-a-full-node',
-        'title': 'Why Run a Full Node',
-        'author': 'Pieter Wuille',
-        'source': 'https://www.reddit.com/r/BitcoinBeginners/comments/3eq3y7/full_node_question/ctk4lnd/',
-        'preview': "One of Bitcoin's strengths - the most important in my opinion even - is the low degree of trust you need in others. If you use a full node for your incoming transactions, you know that there was no cheating anytime in the history of your coins..."
-    },
-    {
-        'id': 1,
-        'month': 12,
         'year': 2017,
         'image_name': 'jimmysong.jpeg',
         'file_name': 'segwit2x-bugs-explained',
         'title': 'Segwit2x Bugs Explained',
         'author': 'Jimmy Song',
         'source': 'https://bitcointechtalk.com/segwit2x-bugs-explained-8e0c286124bc',
+        'source_name': 'Bitcoin Tech Talk',
         'preview': "There was a bug in the Segwit2x software which caused the client to stop at block 494782. Examine the details of what caused the software to stop..."
     },
     {
-        'id': 2,
-        'month': 12,
-        'year': 2017,
+        'id': 1,
+        'day': 15,
+        'month': 9,
+        'year': 2016,
         'image_name': 'starkness.jpg',
         'file_name': 'what-is-the-lightning-network',
         'title': 'What is the Lightning Network and how can it help Bitcoin scale?',
         'author': 'Elizabeth Stark',
         'source': 'https://coincenter.org/entry/what-is-the-lightning-network',
+        'source_name': 'Coin Center',
         'preview': "A plain-language explanation of the Lightning Network, a protocol for scaling and speeding up blockchains..."
-    }
+    },
+    {
+        'id': 0,
+        'day': 29,
+        'month': 7,
+        'year': 2015,
+        'image_name': 'pwuille.png',
+        'file_name': 'why-run-a-full-node',
+        'title': 'Why Run a Full Node',
+        'author': 'Pieter Wuille',
+        'source': 'https://www.reddit.com/r/BitcoinBeginners/comments/3eq3y7/full_node_question/ctk4lnd/',
+        'source_name': 'Reddit',
+        'preview': "One of Bitcoin's strengths - the most important in my opinion even - is the low degree of trust you need in others. If you use a full node for your incoming transactions, you know that there was no cheating anytime in the history of your coins..."
+    },
 ]
